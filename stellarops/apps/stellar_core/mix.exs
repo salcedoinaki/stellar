b3bfec0fcc8dd610d585712b4360d657bd7ee827
isalcedo-stralis <iniaki.salcedo@gmail.com>
2026-01-14 12:42:00 +1000

    Add stellar_core app with initial Mix config
    Introduces the stellar_core application to the monorepo, including its Mix project file and test helper. Updates .gitignore to allow tracking of app files. Adds stream_data as a test/development dependency.

---
+++ stellarops/apps/stellar_core/mix.exs
@@ -0,0 +1,30 @@
+defmodule StellarCore.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :stellar_core,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.15",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {StellarCore.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:stream_data, "~> 1.0", only: [:test, :dev]}
+    ]
+  end
+end
